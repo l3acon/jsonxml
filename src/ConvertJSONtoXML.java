14ca97de460a90d5c9949980741aa68459efe5bf
pwner <l3acon@gmail.com>
2014-08-18 06:17:23 -0600

    works but library won't do

--- src/ConvertJSONtoXML.java
+++ src/ConvertJSONtoXML.java
@@ -2,36 +2,55 @@ import java.util.*;
 import java.io.*;
 
 import org.json.*;
-import com.canes.converters;
+//import com.caanes.converters;
 
-public class ConvertJSONtoXML implements XMLJSONConverterI
+public class ConvertJSONtoXML //implements XMLJSONConverterI
 {
 	public static void main( String[] args)
 		{
+			char [] buffer;
 			try
 			{
-				if(args.length > 0 && args.length < 2)
+				if(args.length > 0 && args.length < 3)
 					{
 						try
 						{
-							BufferedReader reader = new BufferedReader( new FileReader( args[0] ));
-						}
+							InputStream fin = new FileInputStream(args[0]);
+							int fsize = fin.available();
+							buffer = new char [fsize];
+							for( int i = 0; i < fsize; i++)
+							{
+								buffer[i] = (char) fin.read();
+							}
+							fin.close();
+
+							JSONObject json = new JSONObject(new String(buffer) );
+							String xml = XML.toString( json );
+
+							BufferedWriter fout = new BufferedWriter( new FileWriter( args[1] ) );
+							fout.write(xml);
+							fout.close();
+						}  
 						catch( IOException e)
 						{
 							System.out.println(e.getMessage() );
 						}
-
 					}
+				else
+				{
+					PrintUsage();
+				}
 			}
 			catch( IndexOutOfBoundsException e)
 			{
 				System.out.println( e.getMessage() );
 			}
+			return;
 		}
 
 	public static void PrintUsage()
 	{
-		System.out.println( "This is how you use this program");
+		System.out.println("Usage: converter json-file xml-file");
 	}
 }
 
